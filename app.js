c683760e28c5d332b9d204dc84be6e88af8f1faf
Van Tran <5tnvan@gmail.com>
2016-06-01 00:32:15 +0200

    Missing favicon added

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //use middleware
+app.use(favicon(path.join(__dirname, 'public', 'images', 'icons', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
